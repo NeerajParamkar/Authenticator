b89e5ed55adaf5cbfd541f1b4745bd3e90261a84
neerajparamkar <neerajparamkar@gmail.com>
2025-05-20 22:30:52 +0530

    added script

--- server/index.js
+++ server/index.js
@@ -7,10 +7,9 @@ import authRouter from "./Routes/authRoutes.js";
 import userRouter from "./Routes/usersRoutes.js";
 const app=express();
 
-// const allowedOrigin=[process.env.AllOW_LINK]
 
 
-app.use(cors({origin:["https://authenticator-black-eight.vercel.app"],methods:["POST","GET"],credentials:true}));
+app.use(cors({origin:'https://authenticator-black-eight.vercel.app',methods:["POST","GET"],credentials:true}));
 connectDB(); //Database connected/
 app.use(express.json());
 app.use(cookieParser());
