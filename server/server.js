2994958af15f6ebd50b5fb3840eaa3c43c6a515d
neerajparamkar <neerajparamkar@gmail.com>
2025-05-20 17:22:43 +0530

    created ALLOW_LINK in .env file

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app=express();
 const allowedOrigin=[process.env.AllOW_LINK]
 
 
-app.use(cors({origin:allowedOrigin,credentials:true}));
+app.use(cors({origin:allowedOrigin[0],credentials:true}));
 connectDB(); //Database connected/
 app.use(express.json());
 app.use(cookieParser());
